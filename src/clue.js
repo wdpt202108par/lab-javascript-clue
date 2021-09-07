0d6cc28333bf52bbd7d69f3fa6c5b81d86675a9c
Claire Pottier <claire_pottier@hotmail.fr>
2021-09-07 21:44:58 +0200

    Corrections 1st iteration & 2nd iteration

--- src/clue.js
+++ src/clue.js
@@ -4,24 +4,24 @@
 
 const suspectsArray = [
   {
-  firstName: 'Jacob',
-  lastName: 'Green',
-  occupation: 'Entrepreneur',
-  age: 45,
-  description: 'He has a lot of connections',
-  image: 'https://pbs.twimg.com/profile_images/506787499331428352/65jTv2uC.jpeg',
-  color: 'green',
-  }; 
+    firstName: 'Jacob',
+    lastName: 'Green',
+    occupation: 'Entrepreneur',
+    age: 45,
+    description: 'He has a lot of connections',
+    image: 'https://pbs.twimg.com/profile_images/506787499331428352/65jTv2uC.jpeg',
+    color: 'green',
+  },
 
   {
     firstName: 'Doctor',
     lastName: 'Orchid',
     occupation: 'Scientist',
     age: 26,
-    description: 'PhD in plant toxicology. Adopted daughter of Mr. Boddy'
+    description: 'PhD in plant toxicology. Adopted daughter of Mr. Boddy',
     image: 'http://www.radiotimes.com/uploads/images/Original/111967.jpg',
     color: 'white',
-  };
+  },
 
   {
     firstName: 'Victor',
@@ -32,17 +32,17 @@ const suspectsArray = [
     image: 'https://66.media.tumblr.com/ee7155882178f73b3781603f0908617c/tumblr_phhxc7EhPJ1w5fh03_540.jpg',
     color: 'purple',
 
-  };
+  },
 
   {
     firstName: 'Kasandra',
     lastName: 'Scarlet',
     occupation: 'Actor',
     age: 31,
-    description: 'She is an A-list movie star with a dark past'
-    image: 'https://www.radiotimes.com/uploads/images/Original/111967.jpg'
+    description: 'She is an A-list movie star with a dark past',
+    image: 'https://www.radiotimes.com/uploads/images/Original/111967.jpg',
     color: 'red',
-  };
+  },
 
   {
     firstName: 'Eleanor',
@@ -52,67 +52,72 @@ const suspectsArray = [
     description: 'She is from a wealthy family and uses her status and money to earn popularity',
     image: 'https://metrouk2.files.wordpress.com/2016/07/mrs-peacock.jpg',
     color: 'blue',
-  };
+  },
 
-{
-  firstName: 'Jack',
-  lastName: 'Mustard',
-  occupation: 'Retired Football player',
-  age: 62,
-  description: 'He is a former football player who tries to get by on his former glory',
-  image: 'https://static.independent.co.uk/s3fs-public/thumbnails/image/2016/07/04/08/unspecified-3.jpg',
-  color: 'yellow',
-};
+  {
+    firstName: 'Jack',
+    lastName: 'Mustard',
+    occupation: 'Retired Football player',
+    age: 62,
+    description: 'He is a former football player who tries to get by on his former glory',
+    image: 'https://static.independent.co.uk/s3fs-public/thumbnails/image/2016/07/04/08/unspecified-3.jpg',
+    color: 'yellow',
+  },
 
-];
+]
 
 
 // Rooms Array
 
 const roomsArray = [
 
-  {name: Dining Room},
-  {name: Conservatory},
-  {name: Kitchen},
-  {name: Study},
-  {name: Library},
-  {name: Lounge},
-  {name: Billiard Room},
-  {name: Ballroom},
-  {name: Hall},
-  {name: Spa},
-  {name: Living Room},
-  {name: Observatory},
-  {name: Theater},
-  {name: Guest House},
-  {name: Patio},
+  {name: 'Dining Room'},
+  {name: 'Conservatory'},
+  {name: 'Kitchen'},
+  {name: 'Study'},
+  {name: 'Library'},
+  {name: 'Lounge'},
+  {name: 'Billiard Room'},
+  {name: 'Ballroom'},
+  {name: 'Hall'},
+  {name: 'Spa'},
+  {name: 'Living Room'},
+  {name: 'Observatory'},
+  {name: 'Theater'},
+  {name: 'Guest House'},
+  {name: 'Patio'},
 
-];
+]
 
 // Weapons Array
 
 const weaponsArray = [
   
-  {name: rope, weight: 10};
-  {name: knife,weight: 8};
-  {name: candlestick, weight: 2};
-  {name: dumbbell, weight: 30};
-  {name: poison, weight: 2};
-  {name: axe, weight: 15};
-  {name: bat, weight: 13};
-  {name: trophy, weight: 25};
-  {name: pistol, weight: 20};
+  {name: 'rope', weight: 10},
+  {name: 'knife',weight: 8},
+  {name: 'candlestick', weight: 2},
+  {name: 'dumbbell', weight: 30},
+  {name: 'poison', weight: 2},
+  {name: 'axe', weight: 15},
+  {name: 'bat', weight: 13},
+  {name: 'trophy', weight: 25},
+  {name: 'pistol', weight: 20},
   
-  
-];
+]
 
 
 // ITERATION 2
 
-function selectRandom() {}
+function selectRandom(monArray) {
+  const index = Math.trunc(Math.random() * monArray.length);
+  let valeurChoisie = monArray[index];
+  return valeurChoisie;
+}
 
 function pickMystery() {}
-
+//Declare a function named `pickMystery` that takes no arguments and returns an object 
+//with three properties: _suspect_, _weapon_ and _room_, each holding as a value a card of that specific type. 
+//You can get a random card of each type by calling `selectRandom` on each card stack.
 
 // ITERATION 3
 
