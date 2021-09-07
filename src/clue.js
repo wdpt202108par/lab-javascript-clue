9e94b93e92a999f158dc185662328beeaa749de4
hassan sabeh <h.adamsabeh@yahoo.com>
2021-09-07 21:32:05 +0200

    added rooms and weapons

--- src/clue.js
+++ src/clue.js
@@ -76,11 +76,94 @@ const suspectsArray = [
 
 // Rooms Array
 
-const roomsArray = [];
+const roomsArray = [
+  {
+    name: "Dining Room"
+  }, 
+  {
+    name: "Conservatory"
+  },
+  {
+    name: "Kitchen"
+  },
+  {
+    name: "Study"
+  },
+  {
+    name: "Library"
+  },
+  {
+    name: "Billiard Room"
+  },
+  {
+    name: "Lounge"
+  },
+  {
+    name: "Ballroom"
+  },
+  {
+    name: "Hall"
+  },
+  {
+    name: "Spa"
+  },
+  {
+    name: "Living Room"
+  }, 
+  {
+    name: "Observatory"
+  },
+  {
+    name: "Theater"
+  },
+  {
+    name: "Guest House"
+  },
+  {
+    name: "Patio"
+  },
+];
 
 // Weapons Array
 
-const weaponsArray = [];
+const weaponsArray = [
+  {
+    name: "rope",
+   weight: 10
+  },
+  {
+    name: "knife",
+   weight: 8
+  },
+  {
+    name: "candlestick",
+   weight: 2
+  },
+  {
+    name: "dumbbell",
+   weight: 30
+  },
+  {
+    name: "poison",
+   weight: 2
+  },
+  {
+    name: "axe",
+   weight: 15
+  },
+  {
+    name: "bat",
+   weight: 13
+  },
+  {
+    name: "trophy",
+   weight: 25
+  },
+  {
+    name: "pistol",
+   weight: 20
+  },
+];
 
 
 # Clue data
@@ -89,36 +172,6 @@ Use this data to model your game cards into objects.
 
 <br>
 
-### Suspects
-
-
-
-
-
-
-```
-
-```
-
-```
-
-```
-
-<br>
-
-### Weapons
-
-```
-name: rope --- weight: 10
-name: knife --- weight: 8
-name: candlestick --- weight: 2
-name: dumbbell --- weight: 30
-name: poison --- weight: 2
-name: axe --- weight: 15
-name: bat --- weight: 13
-name: trophy --- weight: 25
-name: pistol --- weight: 20
-```
 
 <br>
 
